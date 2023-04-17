a586b834ece8f44bf347d2a7b3ce9e87ca671242
Anthony Schricke <schricke_anthony@yahoo.fr>
2023-04-18 00:44:01 +0200

    [ASC][WORLD_TERRA_INCOGNITA]Update documentation

--- doc/html/search/all_9.js
+++ doc/html/search/all_9.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['screenpresenter_11',['ScreenPresenter',['../classScreen_1_1ScreenPresenter.html',1,'Screen']]],
-  ['screenview_12',['ScreenView',['../classScreen_1_1ScreenView.html',1,'Screen']]],
-  ['screenviewmodel_13',['ScreenViewModel',['../classScreen_1_1ScreenViewModel.html',1,'Screen']]],
+  ['screenpresenter_11',['ScreenPresenter',['../classScreen_1_1ScreenPresenter.html',1,'Screen::ScreenPresenter'],['../classScreenPresenter_1_1ScreenPresenter.html',1,'ScreenPresenter::ScreenPresenter']]],
+  ['screenview_12',['ScreenView',['../classScreenPresenter_1_1ScreenView.html',1,'ScreenPresenter::ScreenView'],['../classScreen_1_1ScreenView.html',1,'Screen::ScreenView']]],
+  ['screenviewmodel_13',['ScreenViewModel',['../classScreen_1_1ScreenViewModel.html',1,'Screen::ScreenViewModel'],['../classScreenPresenter_1_1ScreenViewModel.html',1,'ScreenPresenter::ScreenViewModel']]],
   ['shark_14',['Shark',['../classInteractor_1_1Shark.html',1,'Interactor']]],
   ['survival_15',['Survival',['../classInteractor_1_1Survival.html',1,'Interactor']]]
 ];
